da03d17c143e6c08366cb9c02ce6f813f0c7e9d8
Dmitry <dmitry1939@gmail.com>
2016-02-24 09:18:53 +0200

    header done

--- src/js/partials/develop_1.js
+++ src/js/partials/develop_1.js
@@ -1,14 +1,49 @@
 
 function sendwichFunc(){
 
-    $('.header-sendwich.paused').click(function(){
+    var point = null;
+    var timer = null;
+
+    $('.header-sendwich:not(.paused)').click(function(){
 
         $(this).addClass('paused');
 
         if(!$(this).is('.active')){
+
             $(this).addClass('active');
+            point = 0;
+
+            $('.header-nav').slideDown(300, function(){
+
+                timer = setInterval(function(){
+
+                    $('.header-nav li').eq(point).addClass('show');
+                    point++;
+                    if(point >= $('.header-nav li').length){
+                        clearInterval(timer);
+                        $('.header-sendwich.paused').removeClass('paused');
+                    }
+
+                }, 200);
+            });
+
         }
         else{
+            $(this).removeClass('active');
+            point = $('.header-nav li').length - 1;
+
+            timer = setInterval(function(){
+
+                $('.header-nav li').eq(point).removeClass('show');
+                point--;
+                if(point < 0){
+                    clearInterval(timer);
+                    $('.header-nav').slideUp(300, function(){
+                        $('.header-sendwich.paused').removeClass('paused');
+                    });
+                }
+
+            }, 200);
 
         }
 
