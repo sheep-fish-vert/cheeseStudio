3a6b9b42d28740186f67a0b751d5990a9b625c6f
Art Garm <artem.garmatyk@gmail.com>
2016-02-26 17:07:07 +0200

    iohioo

--- src/js/partials/develop_2.js
+++ src/js/partials/develop_2.js
@@ -2,18 +2,17 @@ $(document).ready(function(){
 
 
 
+
     $('.what-we-doing-button-wrap>.button').click(function() {
+
+        $('.what-we-doing-item').removeClass('show');
+
         var t = $(this);
-        $(this).closest('.what-we-doing-item').children('.tophide').css('top', '0');
-        $(this).closest('.what-we-doing-item').children('.bothide').css('bottom', '0');
 
         setTimeout(function() {
-            t.closest('.what-we-doing-item').removeClass('show');
+
             t.closest('.what-we-doing-item').addClass('show');
 
-            console.log('fffff');
-            t.closest('.what-we-doing-item').children('.tophide').css('top', '-50%');
-            t.closest('.what-we-doing-item').children('.bothide').css('bottom', '-50%');
         }, 1000);
     });
 });
